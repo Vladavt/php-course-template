ba210d03abaf4ec6705a5655fbd8ad794490abc8
Vladavt <vladkorean8@gmail.com>
2025-09-27 12:26:19 +0000

    lab4 | хоббиты

---
+++ sites/skuit/www/lab4/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Автамонов Владислав Алексеевич</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #1f71b4ff;
+            margin: 0;
+            padding: 20px;
+        }
+        h1 {
+            text-align: center;
+            color: #ffffffff;
+        }
+        .block {
+            background: #f0f0f0ff;
+            border: 10px solid #346fbdff;
+            border-radius: 10px;
+            padding: 15px;
+            margin: 15px auto;
+            width: 80%;
+            box-shadow: 10px 10px 6px rgba(0,0,0,0.1);
+        }
+    </style>
+</head>
+<body>
