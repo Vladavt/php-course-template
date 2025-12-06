2b11daf5dfd611706dc93c8c9e4147d4e8f9299e
Vladavt <vladkorean8@gmail.com>
2025-12-06 10:14:09 +0000

    лаба4 привет

--- sites/skuit/www/lab4/header.php
+++ sites/skuit/www/lab4/header.php
@@ -2,26 +2,26 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Автамонов Владислав Алексеевич</title>
+    <title> Автамонов В.А С025</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            background: #1f71b4ff;
+            background: #f4f1ea;
             margin: 0;
             padding: 20px;
         }
         h1 {
             text-align: center;
-            color: #ffffffff;
+            color: #3e2723;
         }
         .block {
-            background: #f0f0f0ff;
-            border: 10px solid #346fbdff;
+            background: #fff;
+            border: 2px solid #d7ccc8;
             border-radius: 10px;
             padding: 15px;
             margin: 15px auto;
             width: 80%;
-            box-shadow: 10px 10px 6px rgba(0,0,0,0.1);
+            box-shadow: 2px 2px 6px rgba(0,0,0,0.1);
         }
     </style>
 </head>
